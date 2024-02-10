b351adad4490be83c4bae082580d237e3a896d8e
tempest <uwh32nkq983ha029@protonmail.com>
2024-02-09 21:40:00 -0800

    qwe

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import logging
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3,5"
 # os.environ["RAY_USE_MULTIPROCESSING_CPU_COUNT"] = "1"
 os.environ['TORCH_CPP_LOG_LEVEL'] = 'INFO'
 
